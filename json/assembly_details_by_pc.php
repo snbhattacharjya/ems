045c77720cfc6d5ce127cb669813b5ecde829cb5
Saikat Narayan Bhattacharjya <snbhattacharjya@gmail.com>
2018-03-15 16:36:15 +0530

    All Assembly at Office Update

--- json/assembly_details_by_pc.php
+++ json/assembly_details_by_pc.php
@@ -7,7 +7,7 @@ require("../config/config.php");
 
 $pccd=$_POST['pc'];
 
-$assembly_details_query="SELECT assemblycd AS AssemblyCode, assemblyname AS AssemblyName FROM assembly WHERE pccd='$pccd' AND subdivisioncd='$subdiv' ORDER BY assemblycd";
+$assembly_details_query="SELECT assemblycd AS AssemblyCode, assemblyname AS AssemblyName FROM assembly ORDER BY assemblycd";
 
 $assembly_details_result=mysqli_query($DBLink,$assembly_details_query) or die(mysqli_error($DBLink));
 $return=array();
