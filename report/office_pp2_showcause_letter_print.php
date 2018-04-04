8614b9d64f2f71a8f77ff25ab908968f806b1404
Saikat Narayan Bhattacharjya <snbhattacharjya@gmail.com>
2018-04-04 13:48:43 +0530

    Personnel Pending Report

--- report/office_pp2_showcause_letter_print.php
+++ report/office_pp2_showcause_letter_print.php
@@ -16,7 +16,6 @@ $code = $_GET['code'];
 	while($res=mysqli_fetch_assoc($query_get_office))
 	{
 ?>
-  <span style="page-break-after: always;"><br></span>
 	<table cellspacing = 0 cellpadding = 5>
     <tr>
   		<th colspan="2"><div>Government of West Bengal<br>
@@ -26,7 +25,7 @@ $code = $_GET['code'];
 	  </th>
     </tr>
 		<tr>
-      <th width="50%" align="left">Memo No: 12(95)</th>
+      <th width="50%" align="left">Memo No: 12(95)/PPCELL(Dist)</th>
       <th width="50%" align="right">Dated: 04/04/2018</th>
 		</tr>
 		<tr>
@@ -57,26 +56,97 @@ $code = $_GET['code'];
       </tr>
       <tr>
         <th colspan="2" align="right">
-		<img src="../img/dm_sign.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br><br>
+		<img src="../img/dm_sign.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
           District Panchayat Election Officer, &  District Magistrate,<br><strong><u>Hooghly</u>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong>
         </th>
       </tr>
       <tr>
         <td colspan="2" align="center">
-  			   <hr>
+  			   <br><br><hr>
         </td>
       </tr>
       <tr>
         <td colspan="2">
-  			     <div align="left">Received the copy with Memo. No.  12(95) Dated: 04/04/2018<br>
+  			     <div align="left">Received the copy with Memo. No. 12(95)/PPCELL(Dist) Dated: 04/04/2018<br>
   			     <strong>OFFICE CODE : <?php echo $res['officecd'];?></strong></div>
   			     <div align="right"> Signature of the receiving officer <br> with phone no. and office seal</div>
         </td>
       </tr>
 	</table>
-  <span style="page-break-after: always;"><br></span>
+  <p style="page-break-after: always"></p>
   <?php
     }
    ?>
+
+   <?php
+   $opt = $_GET['opt'];
+   $code = $_GET['code'];
+   	$query_get_office=mysqli_query($DBLink,"SELECT office.officecd, office.office, office.officer_desg, office.address1, office.address2, block_muni.blockmuni, policestation.policestation, office.postoffice, office.pin, office.email, office.phone, office.mobile, office.tot_staff AS pp1_count, office.male_staff, office.female_staff, office.posted_date FROM (office INNER JOIN block_muni ON block_muni.blockminicd=office.blockormuni_cd) INNER JOIN policestation ON policestation.policestationcd=office.policestn_cd WHERE office.blockormuni_cd='$code' AND officecd NOT IN (SELECT DISTINCT officecd FROM personnel) ORDER BY office.officecd") or die(mysqli_error($DBLink));
+
+   	while($res=mysqli_fetch_assoc($query_get_office))
+   	{
+   ?>
+     <span style="page-break-after: always;"><br></span>
+   	<table cellspacing = 0 cellpadding = 5>
+       <tr>
+     		<th colspan="2"><div>Government of West Bengal<br>
+     			Office of the District Panchayat Election Office & District Magistrate, Hooghly<br>
+     			<u>District Polling Personel Cell, Hooghly</u></div>
+     			<hr style="border : 1px solid black;">
+   	  </th>
+       </tr>
+   		<tr>
+         <th width="50%" align="left">Memo No: 12(95)</th>
+         <th width="50%" align="right">Dated: 04/04/2018</th>
+   		</tr>
+   		<tr>
+   			<th width="50%" align="left">
+   				To <br>
+   				<?php echo $res['officer_desg'];?> <br>
+           <?php echo $res['office'];?> <br>
+   				<?php echo $res['address1'].', '.$res['address2'].'; <strong>Block/Muni</strong> - '.$res['blockmuni'].'; <strong>P.S. </strong>- '.$res['policestation'].'; <strong>PO: </strong>'.$res['postoffice'].'; <strong>Pin </strong>- '.$res['pin'];?>
+   			</th>
+
+     		<th style="width: 50%;">
+     					<strong>Office Code : <?php echo $res['officecd'];?></strong><br>
+     				<strong>Block/Municipality: <?php echo $res['blockmuni'];?></strong>
+     		</span>
+   		</tr>
+       <tr>
+         <th colspan="2">
+           Sub :  Show Cause Notice.
+         </th>
+       </tr>
+   		<tr>
+         <td colspan="2" align="justify">&nbsp;&nbsp;&nbsp;&nbsp;WHEREAS, it has been found that inspite of service of PP1 and PP2 for updating office details and Employees details in connection with performing of Polling Duty to the Panchyayat General Election 2018 under this office memo. No.s 01(1600) / PP Cell (Dist) dated 23.02.2018 and 03(1600) PP Cell (Dist) dated 16.03.2018, you have intentionally and deliberately not updated office details and employees details till date.
+           <br><br>
+           &nbsp;&nbsp;&nbsp;NOW THEREFORE, you are directed to show-cause as to why disciplinary action of the Representation of People Act 1951 will not be taken against you.
+            <br><br>
+     &nbsp;&nbsp;&nbsp;Your written reply should reach the undersigned within 03 (three) days from the date of receipt of this letter. <br>
+
+         </tr>
+         <tr>
+           <th colspan="2" align="right">
+   		<br><img src="../img/dm_sign.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br><br>
+             District Panchayat Election Officer, &  <br>District Magistrate,<strong><u>Hooghly</u>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong>
+           </th>
+         </tr>
+         <tr>
+           <td colspan="2" align="center">
+     			   <hr>
+           </td>
+         </tr>
+         <tr>
+           <td colspan="2">
+     			     <div align="left">Received the copy with Memo. No.  12(95)/PPCELL(Dist) Dated: 04/04/2018<br>
+     			     <strong>OFFICE CODE : <?php echo $res['officecd'];?></strong></div><br><br>
+     			     <div align="right"> Signature of the receiving officer <br> with phone no. and office seal</div>
+           </td>
+         </tr>
+   	</table>
+     <p style="page-break-after: always"></p>
+     <?php
+       }
+      ?>
 </body>
 </html>
--- report/pp2_showcause_report.php
+++ report/pp2_showcause_report.php
@@ -25,11 +25,11 @@ require("../config/config.php");
 $user_id=$_SESSION['UserID'];
 
 $subdiv_names=array();
-$subdiv_query=$mysqli->prepare("SELECT subdivision.subdivisioncd, subdivision.subdivision, block_muni.blockminicd, block_muni.blockmuni, COUNT(personnel.personcd) AS pp2_count FROM ((subdivision INNER JOIN block_muni ON subdivision.subdivisioncd=block_muni.subdivisioncd) INNER JOIN office ON office.blockormuni_cd = block_muni.blockminicd) INNER JOIN personnel ON office.officecd = personnel.officecd AND DATE_FORMAT(personnel.posted_date,'%Y') != '2018' GROUP BY subdivision.subdivisioncd, subdivision.subdivision, block_muni.blockminicd, block_muni.blockmuni ORDER BY subdivision.subdivisioncd") or die($mysqli->error);
+$subdiv_query=$mysqli->prepare("SELECT subdivision.subdivisioncd, subdivision.subdivision, block_muni.blockminicd, block_muni.blockmuni, COUNT(CASE personnel.gender WHEN 'M' THEN 1 END) AS pp2_male_count, COUNT(CASE personnel.gender WHEN 'F' THEN 1 END) AS pp2_female_count FROM ((subdivision INNER JOIN block_muni ON subdivision.subdivisioncd=block_muni.subdivisioncd) INNER JOIN office ON office.blockormuni_cd = block_muni.blockminicd) INNER JOIN personnel ON office.officecd = personnel.officecd AND DATE_FORMAT(personnel.posted_date,'%Y') != '2018' GROUP BY subdivision.subdivisioncd, subdivision.subdivision, block_muni.blockminicd, block_muni.blockmuni ORDER BY subdivision.subdivisioncd") or die($mysqli->error);
 $subdiv_query->execute() or die($subdiv_query->error);
-$subdiv_query->bind_result($subdiv_code,$subdiv_name,$block_muni_code,$block_muni_name,$pp2_count) or die($subdiv_query->error);
+$subdiv_query->bind_result($subdiv_code,$subdiv_name,$block_muni_code,$block_muni_name,$pp2_male_count,$pp2_female_count) or die($subdiv_query->error);
 while($subdiv_query->fetch()){
-	$subdiv_names[]=array("SubDivCode"=>$subdiv_code,"SubDivName"=>$subdiv_name,"BlockMuniCode"=>$block_muni_code,"BlockMuniName"=>$block_muni_name,"PP2Count"=>$pp2_count);
+	$subdiv_names[]=array("SubDivCode"=>$subdiv_code,"SubDivName"=>$subdiv_name,"BlockMuniCode"=>$block_muni_code,"BlockMuniName"=>$block_muni_name,"PP2MaleCount"=>$pp2_male_count,"PP2FemaleCount"=>$pp2_female_count);
 }
 $subdiv_query->close();
 ?>
@@ -38,14 +38,15 @@ $subdiv_query->close();
                   <th>Sl No</th>
                   <th>Subdivision</th>
                   <th>Block/Municipality</th>
-                  <th>PP2 Not Updated</th>
+                  <th>PP2 Male Not Updated</th>
+                  <th>PP2 Female Not Updated</th>
                   <th>Office Show Cause Letter</th>
                 </tr>
               </thead>
               <tbody>
                 <?php
-                $total_pp1 = 0;
-                $total_pp2 = 0;
+                $total_male_pp2 = 0;
+                $total_female_pp2 = 0;
 	for($i=0;$i<count($subdiv_names);$i++){
 ?>
                   <tr>
@@ -55,10 +56,12 @@ $subdiv_query->close();
 
                     <td><?php echo $subdiv_names[$i]['SubDivName']; ?></td>
                     <td><?php echo $subdiv_names[$i]['BlockMuniName']; ?></td>
-                    <td><?php echo $subdiv_names[$i]['PP2Count']; ?></td>
-                    <td><?php echo "<a href='office_pp2_showcause_letter_print.php?opt=blockmuni&code=".$subdiv_names[$i]['BlockMuniCode']."' target='_blank' class='text-red'><i class='fa fa-print'></i> print</a>"; ?></td>
+                    <td><?php echo $subdiv_names[$i]['PP2MaleCount']; ?></td>
+                    <td><?php echo $subdiv_names[$i]['PP2FemaleCount']; ?></td>
+                    <td><?php echo "<a href='report/office_pp2_showcause_letter_print.php?opt=blockmuni&code=".$subdiv_names[$i]['BlockMuniCode']."' target='_blank' class='text-red'><i class='fa fa-print'></i> print</a>"; ?></td>
                     <?php
-                      $total_pp2 += $subdiv_names[$i]['PP2Count'];
+                      $total_male_pp2 += $subdiv_names[$i]['PP2MaleCount'];
+                      $total_female_pp2 += $subdiv_names[$i]['PP2FemaleCount'];
                     ?>
                   </tr>
                   <?php
@@ -68,7 +71,8 @@ $subdiv_query->close();
               <tfoot>
                 <tr class="bg-danger">
                   <th colspan="3">Total</th>
-                  <th><?php echo $total_pp2; ?></th>
+                  <th><?php echo $total_male_pp2; ?></th>
+                  <th><?php echo $total_female_pp2; ?></th>
                   <th>&nbsp;</th>
                 </tr>
               </tfoot>
