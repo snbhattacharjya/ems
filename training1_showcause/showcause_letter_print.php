a89224accd81ae0dfebddce5b336b2dbbd1bc532
Saikat Narayan Bhattacharjya <snbhattacharjya@gmail.com>
2018-04-30 20:32:27 +0530

    Training 1 Showcause Letter

--- training1_showcause/showcause_letter_print.php
+++ training1_showcause/showcause_letter_print.php
@@ -6,7 +6,7 @@ session_start();
 if(!isset($_SESSION['UserID']))
     die("Login Expired!. Please Login again to continue");
 require("../config/config.php");
-//include "../phpqrcode/qrlib.php"; 
+//include "../phpqrcode/qrlib.php";
 $type=$_GET['type'];
 
 $env_query=$mysqli->prepare("SELECT environment, distnm_sml, apt1_orderno, apt1_date FROM environment") or die($mysqli->error);
@@ -21,33 +21,33 @@ if($type == 'date_venue'){
     $training_time=$_GET['training_time'];
     $training_type=$_GET['training_type'];
     $blockmuni=$_GET['blockmuni'];
-    
-    $first_showcause_query=$mysqli->prepare("SELECT personnel.personcd, personnel.officer_name, personnel.off_desg, poststat.poststatus, personnel.mob_no, office.officecd, office.office, CONCAT(office.address1,', ',office.address2), block_muni.blockmuni, office.postoffice, subdivision.subdivision, policestation.policestation, district.district, office.pin, training_venue.venuename, CONCAT(training_venue.venueaddress1,', ',training_venue.venueaddress2), training_schedule.training_dt, training_schedule.training_time, show_cause_memo.memo_no, show_cause_memo.memo_date FROM (((((((((office INNER JOIN district ON office.districtcd = district.districtcd) INNER JOIN subdivision ON office.subdivisioncd = subdivision.subdivisioncd) INNER JOIN block_muni ON block_muni.blockminicd=office.blockormuni_cd) INNER JOIN policestation ON policestation.policestationcd=office.policestn_cd) INNER JOIN personnel ON office.officecd=personnel.officecd) INNER JOIN poststat ON poststat.post_stat = personnel.poststat) INNER JOIN training_schedule ON personnel.training1_sch = training_schedule.schedule_code) INNER JOIN training_venue ON training_schedule.training_venue = training_venue.venue_cd) INNER JOIN personnel_training_absent ON personnel.personcd = personnel_training_absent.personcd) INNER JOIN show_cause_memo ON DATE(training_schedule.training_dt) = show_cause_memo.training_date WHERE personnel_training_absent.training_type = ? AND personnel_training_absent.training_date = ? AND personnel_training_absent.training_time = ? AND training_venue.venue_base_name = ? AND office.blockormuni_cd = ? AND personnel.personcd NOT IN (SELECT personcd FROM personnel_exempt_post_random) ORDER BY office.officecd, personnel.personcd") or die($mysqli->error);
+
+    $first_showcause_query=$mysqli->prepare("SELECT personnel.personcd, personnel.officer_name, personnel.off_desg, poststat.poststatus, personnel.mob_no, office.officecd, office.office, CONCAT(office.address1,', ',office.address2), block_muni.blockmuni, office.postoffice, subdivision.subdivision, policestation.policestation, district.district, office.pin FROM ((((((office INNER JOIN district ON office.districtcd = district.districtcd) INNER JOIN subdivision ON office.subdivisioncd = subdivision.subdivisioncd) INNER JOIN block_muni ON block_muni.blockminicd=office.blockormuni_cd) INNER JOIN policestation ON policestation.policestationcd=office.policestn_cd) INNER JOIN personnel ON office.officecd=personnel.officecd) INNER JOIN poststat ON poststat.post_stat = personnel.poststat) INNER JOIN personnel_training_absent ON personnel.personcd = personnel_training_absent.personcd WHERE personnel_training_absent.training_type = ? AND personnel_training_absent.training_date = ? AND personnel_training_absent.training_time = ? AND training_venue.venue_base_name = ? AND office.blockormuni_cd = ? AND personnel.personcd NOT IN (SELECT personcd FROM personnel_exempt_post_random) ORDER BY office.officecd, personnel.personcd") or die($mysqli->error);
 $first_showcause_query->bind_param("sssss",$training_type,$training_date,$training_time,$training_venue,$blockmuni) or die($first_showcause_query->error);
 }
 
 if($type == 'subdiv'){
     $subdiv=$_GET['subdiv'];
     $training_type=$_GET['training_type'];
-    
-    $first_showcause_query=$mysqli->prepare("SELECT personnel.personcd, personnel.officer_name, personnel.off_desg, poststat.poststatus, personnel.mob_no, office.officecd, office.office, CONCAT(office.address1,', ',office.address2), block_muni.blockmuni, office.postoffice, subdivision.subdivision, policestation.policestation, district.district, office.pin, training_venue.venuename, CONCAT(training_venue.venueaddress1,', ',training_venue.venueaddress2), training_schedule.training_dt, training_schedule.training_time, show_cause_memo.memo_no, show_cause_memo.memo_date FROM (((((((((office INNER JOIN district ON office.districtcd = district.districtcd) INNER JOIN subdivision ON office.subdivisioncd = subdivision.subdivisioncd) INNER JOIN block_muni ON block_muni.blockminicd=office.blockormuni_cd) INNER JOIN policestation ON policestation.policestationcd=office.policestn_cd) INNER JOIN personnel ON office.officecd=personnel.officecd) INNER JOIN poststat ON poststat.post_stat = personnel.poststat) INNER JOIN training_schedule ON personnel.training1_sch = training_schedule.schedule_code) INNER JOIN training_venue ON training_schedule.training_venue = training_venue.venue_cd) INNER JOIN personnel_training_absent ON personnel.personcd = personnel_training_absent.personcd) INNER JOIN show_cause_memo ON DATE(training_schedule.training_dt) = show_cause_memo.training_date WHERE personnel_training_absent.training_type = ? AND office.subdivisioncd = ? AND personnel.personcd NOT IN (SELECT personcd FROM personnel_exempt_post_random) ORDER BY office.officecd, personnel.personcd") or die($mysqli->error);
+
+    $first_showcause_query=$mysqli->prepare("SELECT personnel.personcd, personnel.officer_name, personnel.off_desg, poststat.poststatus, personnel.mob_no, office.officecd, office.office, CONCAT(office.address1,', ',office.address2), block_muni.blockmuni, office.postoffice, subdivision.subdivision, policestation.policestation, district.district, office.pin FROM ((((((office INNER JOIN district ON office.districtcd = district.districtcd) INNER JOIN subdivision ON office.subdivisioncd = subdivision.subdivisioncd) INNER JOIN block_muni ON block_muni.blockminicd=office.blockormuni_cd) INNER JOIN policestation ON policestation.policestationcd=office.policestn_cd) INNER JOIN personnel ON office.officecd=personnel.officecd) INNER JOIN poststat ON poststat.post_stat = personnel.poststat) INNER JOIN personnel_training_absent ON personnel.personcd = personnel_training_absent.personcd WHERE personnel_training_absent.training_type = ? AND office.subdivisioncd = ? AND personnel.personcd NOT IN (SELECT personcd FROM personnel_exempt_post_random) ORDER BY office.officecd, personnel.personcd") or die($mysqli->error);
 $first_showcause_query->bind_param("ss",$training_type,$subdiv) or die($first_showcause_query->error);
 }
 
 if($type == 'blockmuni'){
     $training_type=$_GET['training_type'];
     $blockmuni=$_GET['blockmuni'];
-    
-    $first_showcause_query=$mysqli->prepare("SELECT personnel.personcd, personnel.officer_name, personnel.off_desg, poststat.poststatus, personnel.mob_no, office.officecd, office.office, CONCAT(office.address1,', ',office.address2), block_muni.blockmuni, office.postoffice, subdivision.subdivision, policestation.policestation, district.district, office.pin, training_venue.venuename, CONCAT(training_venue.venueaddress1,', ',training_venue.venueaddress2), training_schedule.training_dt, training_schedule.training_time, show_cause_memo.memo_no, show_cause_memo.memo_date FROM (((((((((office INNER JOIN district ON office.districtcd = district.districtcd) INNER JOIN subdivision ON office.subdivisioncd = subdivision.subdivisioncd) INNER JOIN block_muni ON block_muni.blockminicd=office.blockormuni_cd) INNER JOIN policestation ON policestation.policestationcd=office.policestn_cd) INNER JOIN personnel ON office.officecd=personnel.officecd) INNER JOIN poststat ON poststat.post_stat = personnel.poststat) INNER JOIN training_schedule ON personnel.training1_sch = training_schedule.schedule_code) INNER JOIN training_venue ON training_schedule.training_venue = training_venue.venue_cd) INNER JOIN personnel_training_absent ON personnel.personcd = personnel_training_absent.personcd) INNER JOIN show_cause_memo ON DATE(training_schedule.training_dt) = show_cause_memo.training_date WHERE personnel_training_absent.training_type = ? AND office.blockormuni_cd = ? AND personnel.personcd NOT IN (SELECT personcd FROM personnel_exempt_post_random) ORDER BY office.officecd, personnel.personcd") or die($mysqli->error);
+
+    $first_showcause_query=$mysqli->prepare("SELECT personnel.personcd, personnel.officer_name, personnel.off_desg, poststat.poststatus, personnel.mob_no, office.officecd, office.office, CONCAT(office.address1,', ',office.address2), block_muni.blockmuni, office.postoffice, subdivision.subdivision, policestation.policestation, district.district, office.pin FROM ((((((office INNER JOIN district ON office.districtcd = district.districtcd) INNER JOIN subdivision ON office.subdivisioncd = subdivision.subdivisioncd) INNER JOIN block_muni ON block_muni.blockminicd=office.blockormuni_cd) INNER JOIN policestation ON policestation.policestationcd=office.policestn_cd) INNER JOIN personnel ON office.officecd=personnel.officecd) INNER JOIN poststat ON poststat.post_stat = personnel.poststat) INNER JOIN personnel_training_absent ON personnel.personcd = personnel_training_absent.personcd WHERE personnel_training_absent.training_type = ? AND office.blockormuni_cd = ? AND personnel.personcd NOT IN (SELECT personcd FROM personnel_exempt_post_random) ORDER BY office.officecd, personnel.personcd") or die($mysqli->error);
 $first_showcause_query->bind_param("ss",$training_type,$blockmuni) or die($first_showcause_query->error);
 }
 
 $first_showcause_query->execute() or die($first_showcause_query->error);
-$first_showcause_query->bind_result($personcd, $officer_name, $off_desg, $poststatus, $mob_no, $officecd, $office, $address, $block_muni_name, $postoffice, $subdivision, $policestation, $district, $pin, $venuename, $venueaddress, $training_dt, $training_time,$memo_no,$memo_date) or die($first_showcause_query->error);
+$first_showcause_query->bind_result($personcd, $officer_name, $off_desg, $poststatus, $mob_no, $officecd, $office, $address, $block_muni_name, $postoffice, $subdivision, $policestation, $district, $pin) or die($first_showcause_query->error);
 
 $pp_data=array();
 while($first_showcause_query->fetch()){
-    $pp_data[]=array("personcd"=>$personcd, "officer_name"=>$officer_name, "off_desg"=>$off_desg, "poststatus"=>$poststatus, "mob_no"=>$mob_no, "officecd"=>$officecd, "office"=>$office, "address"=>$address, "block_muni_name"=>$block_muni_name, "postoffice"=>$postoffice, "subdivision"=>$subdivision, "policestation"=>$policestation, "district"=>$district, "pin"=>$pin, "venuename"=>$venuename, "venueaddress"=>$venueaddress, "training_dt"=>$training_dt, "training_time"=>$training_time,"memo_no"=>$memo_no,"memo_date"=>$memo_date);
+    $pp_data[]=array("personcd"=>$personcd, "officer_name"=>$officer_name, "off_desg"=>$off_desg, "poststatus"=>$poststatus, "mob_no"=>$mob_no, "officecd"=>$officecd, "office"=>$office, "address"=>$address, "block_muni_name"=>$block_muni_name, "postoffice"=>$postoffice, "subdivision"=>$subdivision, "policestation"=>$policestation, "district"=>$district, "pin"=>$pin);
 }
 $first_showcause_query->close();
 for($i = 0;$i < count($pp_data); $i++){
@@ -57,23 +57,24 @@ for($i = 0;$i < count($pp_data); $i++){
         <th colspan="2">
             <img src="../pp_training/indian-symbol4.jpg" alt=""/><br>
             GOVERNMENT OF WEST BENGAL<br>
-            Office of the District Election Officer & District Magistrate, Hooghly<br>
+            Office of the District Panchayat Election Officer & District Magistrate, Hooghly<br>
             District Polling Personnel Cell<br>
+            Email: ppcell.hooghly@gmail.com
         </th>
     </tr>
     <tr>
         <td width="50%" style="padding-top: 15;">
-            No. <?php echo $pp_data[$i]['memo_no']; ?>/PPCell(Dist)
+            Memo No. 50/1(2)(1400) / PP Cell (Dist)
         </td>
         <td width="50%" align="right" style="padding-top: 15;">
-            Date: <?php echo date_format(date_create($pp_data[$i]['memo_date']),"d/m/Y"); ?>
+            Date: 30.04.2018
         </td>
     </tr>
     <tr>
         <th colspan="2" style="padding-top: 15; text-align: left">
             To<br>
             <?php echo $pp_data[$i]['officer_name']."<br>".$pp_data[$i]['off_desg']."<br>PIN - ".$pp_data[$i]['personcd']; ?><br>
-            <?php echo $pp_data[$i]['office']." (".$pp_data[$i]['officecd']."), <br>".$pp_data[$i]['address']." <br>P.O. - ".$pp_data[$i]['postoffice'].", Subdiv - ".$pp_data[$i]['subdivision'].", <br>P.S. - ".$pp_data[$i]['policestation'].", <br>Dist - ".$pp_data[$i]['district'].", Pincode - ".$pp_data[$i]['pin']; ?>   
+            <?php echo $pp_data[$i]['office']." (".$pp_data[$i]['officecd']."), <br>".$pp_data[$i]['address']." <br>P.O. - ".$pp_data[$i]['postoffice'].", Subdiv - ".$pp_data[$i]['subdivision'].", <br>P.S. - ".$pp_data[$i]['policestation'].", <br>Dist - ".$pp_data[$i]['district'].", Pincode - ".$pp_data[$i]['pin']; ?>
         </th>
     </tr>
     <tr>
@@ -83,48 +84,50 @@ for($i = 0;$i < count($pp_data); $i++){
     </tr>
     <tr>
         <td colspan="2" style="padding-top: 20; text-align: justify">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;WHEREAS, it has been found that in spite of service of 1st order of Appointment for performing duty of <strong><?php echo $pp_data[$i]['poststatus']; ?></strong>  to the Assembly General Election, 2016 under this office order no. 65/PPcell/HOOG(24907) dated. 12/03/2016, you have intentionally and deliberately kept yourself absent in the Training Venue - <strong><?php echo $pp_data[$i]['venuename']; ?></strong> on <strong><?php echo date_format(date_create($pp_data[$i]['training_dt']),"d-M-Y"); ?></strong> at <strong><?php echo $pp_data[$i]['training_time']; ?></strong>.
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;WHEREAS, it has been found that inspite of service of appointment letter for performing duty of  <strong><?php echo $pp_data[$i]['poststatus']; ?></strong>  to the Panchayat General Election 2018, under this office Order No.21/P.P. Cell Dist (24525) dated. 09/04/2018, you have intentionally and deliberately kept yourself absent from attending the scheduled Training Programme leading to serious dislocation of the entire election process.
         </td>
     </tr>
     <tr>
         <td colspan="2" style="padding-top: 15; text-align: justify">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NOW THEREFORE, you are directed to show-cause as to why disciplinary action as per provisions under Section 28A/134 of the Representation of People Act' 1951 will not be taken against you in consultation with your administrative department.
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NOW, THEREFORE, you are directed to Show-Cause as to why action as per provisions under Section 28A/134 of the Representation of People Act, 1951 will not be taken against you.
         </td>
     </tr>
     <tr>
         <td colspan="2" style="padding-top: 15; text-align: justify">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Your written reply should reach the undersigned within 03 (three) days from the date of receipt of this letter.
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Your written reply shall have to reach the concerned Sub Divisional Officer within two days from the date of receipt of this letter.
         </td>
     </tr>
     <tr>
         <td colspan="2" style="padding-top: 15; text-align: justify">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You are further directed to attend Mop-Up training as per training schedule attached, failing which appropriate penal measure will be taken against you without any further correspondence from this end.
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You are further directed to attend the 2nd training programme positively to be held on 06.05.2018 as per training venue attached with the 2nd appointment letter, failing which appropriate penal measure will be taken against you without any further correspondence from this end.
         </td>
     </tr>
     <tr>
         <th style="padding-top: 40; text-align: right" colspan="2">
             <img src="../pp_training/dm-sign1.jpg" alt="" style="padding-right: 30px"/><br>
-            District Election Officer &<br>
+            District Panchayat Election Officer, &<br>
             District Magistrate Hooghly
         </th>
     </tr>
     <tr>
         <td width="50%" style="padding-top: 15;">
-            No. <?php echo $pp_data[$i]['memo_no']; ?>/PPCell(Dist)
+            Memo No. 50/1(2)(1400) / PP Cell (Dist)
         </td>
         <td width="50%" align="right" style="padding-top: 15;">
-            Date: <?php echo date_format(date_create($pp_data[$i]['memo_date']),"d/m/Y"); ?>
+            Date: 30.04.2018
         </td>
     </tr>
     <tr>
         <td colspan="2" style="padding-top: 20">
-            1.	The Sponsoring Authority with a request to serve the same to the above mentioned employee and direct him to submit reply within stipulated time and attend mop-up training on the scheduled date and time without fail.
+          Copy forwarded for information to:<br>
+          1. The Sponsoring Authority to serve the same to the above mentioned employee.  A consolidated reply should reach the concerned subdivision within two days from the date of receipt of this show cause notice.<br>
+          2. The Sub-divisional Officer, Arambagh/ Chandannagore/ Serampore/ Sadar for information and taking necessary action.
         </td>
     </tr>
     <tr>
         <th style="padding-top: 40; text-align: right" colspan="2">
             <img src="../pp_training/dm-sign1.jpg" alt="" style="padding-right: 30px"/><br>
-            District Election Officer &<br>
+            District Panchayat Election Officer, &<br>
             District Magistrate Hooghly
         </th>
     </tr>
